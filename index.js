ea7c8f22eaddd1cfbb04fcb451b556d5b9847d13
Lucas Santana <lucas.diassantana@gmail.com>
2022-07-31 19:18:43 -0300

    created sounds folder, auth.json. modified index.js and package.json

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const { Client, Intents } = require('discord.js')
-const client = new Client({ intents: 32767 })
+const client = new Client({
+    intents: [Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MEMBERS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_VOICE_STATES]
+})
 const { joinVoiceChannel, createAudioResource } = require('@discordjs/voice');
 const { createAudioPlayer } = require('@discordjs/voice')
-const prefix = '!'
+const auth = require('./auth.json')
 const config = require('./config.json')
-const token = config.token;
+const prefix = config.prefix
+const token = auth.token;
 
 client.login(token)
 
 client.once("ready", () => {
     console.log("The bot is now online!");
-    client.user.setActivity("Online", {type: "WATCHING"})
+    client.user.setActivity("Online e roteando!", { type: "WATCHING" })
 })
 // Ping Pong
 client.on("messageCreate", (message) => {
@@ -18,6 +24,26 @@ client.on("messageCreate", (message) => {
         message.channel.send("pong!");
     }
 });
+
+// Join
+client.on('messageCreate', async message => {
+    function isCommand(command) {
+        return !!message.content.toLowerCase().startsWith(prefix + command);
+    };
+
+    if (isCommand('join')) {
+        const channel = message.member.voice.channel;
+        const connection = joinVoiceChannel({
+            channelId: channel.id,
+            guildId: message.guild.id,
+            adapterCreator: message.guild.voiceAdapterCreator
+        })
+
+        if (!channel)
+            return interaction.reply({ content: "⛔ Você deve estar em um canal de voz.", ephemeral: true })
+    }
+})
+
 // Snore
 client.on('messageCreate', async message => {
     function isCommand(command) {
@@ -26,11 +52,11 @@ client.on('messageCreate', async message => {
 
     if (isCommand('sleep')) {
         const channel = message.member.voice.channel;
-        if (!channel) 
-        return interaction.reply({content: "⛔ Você deve estar em um canal.", ephemeral: true})
+        if (!channel)
+            return interaction.reply({ content: "⛔ Você deve estar em um canal de voz.", ephemeral: true })
 
         const player = createAudioPlayer();
-        const resource = createAudioResource('D:/Programação/Projetos/SleepBot/snore.mp3')
+        const resource = createAudioResource('sounds/snore.mp3')
 
         const connection = joinVoiceChannel({
             channelId: channel.id,
@@ -38,14 +64,8 @@ client.on('messageCreate', async message => {
             adapterCreator: message.guild.voiceAdapterCreator
         })
 
-        player.play(resource, {volume: 0.1})
+        player.play(resource)
         connection.subscribe(player);
-
-        const subscription = connection.subscribe(player);
-        if (subscription) {
-            // Unsubscribe after 5 seconds (stop playing audio on the voice connection)
-            setTimeout(() => subscription.unsubscribe(), 5_000);
-        }
     }
 })
 
