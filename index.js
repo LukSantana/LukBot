32ce8b40d50f8ea340f9fc674ff61e134ef93569
Lucas Santana <lucas.diassantana@gmail.com>
2022-09-12 22:08:27 -0300

    Fixed activity error

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const { Client, GatewayIntentBits, Collection, ActivityType } = require('discord.js');
 const { Player } = require("discord-player")
 
 const fs = require('fs');
@@ -66,52 +66,8 @@ client.login(process.env.TOKEN);
 
 client.once("ready", () => {
     console.log("O LukBot está online e roteando, bebês!!");
-    client.user.setActivity("Online e roteando!", { type: "PLAYING" })
+    client.user.setPresence({
+        activities: [{ name: `Online e roteando, bebês`, type: ActivityType.Watching }],
+        status: 'dnd',
+    })
 })
-
-// export default client;
-
-// // Import Functions
-// import playAudio from './commands/playAudio.js'
-// import commands from './commands/commands.js.js'
-// import leaveChannel from './commands/leave.js.js'
-
-// // Import commands data
-// import data from './config/data.js'
-
-// // Snore
-// let snoreCommand = data.filter((command) => command.name === 'Sleep')[0]
-// playAudio(snoreCommand.audio, snoreCommand.command)
-
-// // Nyo
-// let nyoCommand = data.filter((command) => command.name === 'Nyo')[0]
-// playAudio(nyoCommand.audio, nyoCommand.command)
-
-// // Hamoud
-// let hamoudCommand = data.filter((command) => command.name === 'Hamoud')[0]
-// playAudio(hamoudCommand.audio, hamoudCommand.command)
-
-
-// // Palmeiras
-// let palmeirasCommand = data.filter((command) => command.name === 'Palmeiras')[0]
-// playAudio(palmeirasCommand.audio, palmeirasCommand.command)
-
-// // Flamengo
-// let flamengoCommand = data.filter((command) => command.name === 'Flamengo')[0]
-// playAudio(flamengoCommand.audio, flamengoCommand.command)
-
-// // Brasil
-// let brasilCommand = data.filter((command) => command.name === 'Brasil')[0]
-// playAudio(brasilCommand.audio, brasilCommand.command)
-
-// // Brasil
-// let atumalacaCommand = data.filter((command) => command.name === 'Atumalaca')[0]
-// playAudio(atumalacaCommand.audio, atumalacaCommand.command)
-
-// // Commands
-// let helpCommand = "comandos"
-// commands(helpCommand)
-
-// //Leave
-// let leaveCommand = "leave"
-// leaveChannel(leaveCommand)
